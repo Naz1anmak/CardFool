b73c3d160b18082f36e856e366dc3979f59c7c12
Alexander <fign.9polna@yandex.ru>
2025-05-14 19:32:13 +0300

    ver 0.1

--- CardFool/MPlayer1.cs
+++ CardFool/MPlayer1.cs
@@ -20,32 +20,59 @@ namespace CardFool
         // количество карт на руке
         public int GetCount()
         {
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            return hand.Count;
         }
         // Добавляет новую карту в руку
         public void AddToHand(SCard card)
         {
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            hand.Add(card);
         }
 
         // Сделать ход (первый)
         public List<SCard> LayCards()
         {
-           //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            List < SCard > cards = new List < SCard >();
+            cards.Add(hand[0]);
+            hand.Remove(hand[0]);
+            return cards;
         }
 
         // Отбиться.
         // На вход подается набор карт на столе, часть из них могут быть уже покрыты
         public bool Defend(List<SCardPair> table)
         {
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Suits suit;
+            int rank;
+            bool answer = false;
+
+            foreach (SCardPair pair in table)
+            {
+                if (pair.Beaten) continue;
+
+                suit = pair.Down.Suit;
+                rank = pair.Down.Rank;
+
+                foreach (SCard card in hand)
+                {
+                    if ((card.Suit == suit && card.Rank > rank)
+                        || (card.Suit == MTable.GetTrump().Suit && card.Rank > rank))
+                    {
+                        answer = pair.SetUp(pair.Up, MTable.GetTrump().Suit);
+                        hand.Remove(card);
+                        return answer;
+                    }
+                }
+            }
+
+            return answer;
         }
 
         // Подбросить карты
         // На вход подаются карты на столе
         public bool AddCards(List<SCardPair> table)
         {
-           
+            if (hand.Count > 0) return true;
+            return false;
         }
         
         // Вывести в консоль карты на руке
